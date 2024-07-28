b06147b9f55d26ac06f7ef3ce71bfe8179f2942a
enceladus <aryanhazarika04@gmail.com>
2024-07-29 02:01:10 +0530

    ADD: scrollTrigger to project cards

--- src/routes/projects/Card.svelte
+++ src/routes/projects/Card.svelte
@@ -4,7 +4,8 @@
 		Members: string[],
 		ID: number,
 		GithubLink: string,
-		PaperLink: string;
+		PaperLink: string,
+		cardClass: string;
 
 	function openPaper() {
 		setTimeout(() => {
@@ -20,18 +21,20 @@
 </script>
 
 <div
-	class={`card max-w-[700px] aspect-[4/1.6] rounded-sm p-4 mb-10 border border-dashed mx-auto ${
+	class={`projectCard max-w-[700px] aspect-[4/1.6] rounded-sm p-4 mb-10 border border-dashed mx-auto ${
 		ID % 2 === 0 ? 'lg:ml-auto lg:mr-0' : 'lg:mr-auto lg:ml-0'
-	}`}
+	} ${cardClass}`}
 >
-	<p class="pb-2 text-2xl font-bold text-[#FFFCE1]">{ProjectName}</p>
+	<p class="pb-2 text-2xl font-bold text-[#FFFCE1]">
+		{ProjectName}
+	</p>
 
-	<p class="pb-2 text-[#FFFCE1]/80">
+	<p class="pb-2 text-[#FFFCE1]/70">
 		{ProjectDesc}
 	</p>
 
 	<div class="grid grid-cols-[auto_auto] max-w-fit pb-3">
-		<p class="text-[#FFFCE1]/80">Members:</p>
+		<p class="text-[#FFFCE1]/70">Members:</p>
 		<div class="pl-2">
 			<p class="text-[#FFFCE1]/80 inline italic">{Members.join(', ')}</p>
 		</div>
